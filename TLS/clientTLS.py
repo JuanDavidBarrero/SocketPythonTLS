0ed54d87ec72326670995f1735cc5d41629cb381
JuanDavid <Juandbarrero@hotmail.com>
2023-01-16 15:08:08 -0500

    updated clientTLS

--- TLS/clientTLS.py
+++ TLS/clientTLS.py
@@ -4,10 +4,7 @@ import ssl
 HOST = "192.168.1.94"  
 PORT = 65432 
 
-
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
-context.verify_mode = ssl.CERT_OPTIONAL 
-context.check_hostname = False
 context.load_verify_locations(cafile="certs\ca.crt")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
